89a83c92224c8a5a2962800b461f700063b6662d
Nora Luna <nob_cat@stargate.cat.pdx.edu>
2022-11-30 20:14:55 -0800

    functional?

--- combs.py
+++ combs.py
@@ -5,9 +5,15 @@ from itertools import product
 match = "0badbeef"
 current = digits+ascii_letters
 
-for n in range(1, 4+1):
+for n in range(1, 100):
     for comb in product(current, repeat=n):
-        print ("testing",''.join(comb))
+        print(''.join(comb) + "d470d406")
+        testing = ''.join(comb) + "d470d406"
+        hash = hashlib.md5(testing.encode('ascii')).hexdigest()
+        if hash[:8] == match:
+            print('Match:"' + testing + '", md5:' + hash)
+            break
+        print (''.join(comb))
 
 #while True:
 #    testing = str(current) + "d470d406"
