5005427b8168dff5cafcdacbaf07103e1dc79880
Nora Luna <nob_cat@stargate.cat.pdx.edu>
2022-11-30 20:02:58 -0800

    string creation added

--- combs.py
+++ combs.py
@@ -1,12 +1,19 @@
 import hashlib
+from string import digits, ascii_letters
+from itertools import product
 
-match = '0badbeef'
-current = 0
-while True:
-    testing = str(candidate) + "d470d406"
-    hash = hashlib.md5(testing.encode('ascii')).hexdigest()
-    if hash[:8] == match:
-        print('Match:"' + testing + '", md5:' + hash)
-        break
-    current = current + 1
-    print(current)
+match = "0badbeef"
+current = digits+ascii_letters
+
+for n in range(1, 4+1):
+    for comb in product(current, repeat=n):
+        print ("testing",''.join(comb))
+
+#while True:
+#    testing = str(current) + "d470d406"
+#    hash = hashlib.md5(testing.encode('ascii')).hexdigest()
+#    if hash[:8] == match:
+#        print('Match:"' + testing + '", md5:' + hash)
+#        break
+#    current = current + 'a'
+#    print(current, " ", hash)
